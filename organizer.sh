6ab89601e9668f78ff88240414d156b50a90fc15
Panom M <p.makuei@alustudent.com>
2025-11-19 17:16:35 +0200

    --Initializing the  shell script file---

---
+++ organizer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIRECTORY="./archive"
+LOG_FILE="organizer.log"
+
+mkdir -p "$DIRECTORY"
+
+# Check CSV files
+csv_file=(*.csv)
+
+if [ ! -f "${csv_file[0]}" ]; then
+    echo "No CSV files available"
+    exit 
+fi
+
+for file in *.csv; do
+    timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+    new_filename="$file-${timestamp}.csv"
+
+    {
+        echo "Archiving Details: $file"
+        echo "Time Archived: $timestamp"
+        echo "New File Name: $new_filename"
+        echo "Content:"
+        cat "$file"
+        echo ""
+    } >> "organizer.log"
+
+    mv "$file" "$DIRECTORY/$new_filename"
+done
